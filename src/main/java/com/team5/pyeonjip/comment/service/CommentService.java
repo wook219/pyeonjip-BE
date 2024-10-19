43e6636b00a455f152651a6b911411ad290286a8
[CLOUD4] 신영광 <halogod9985@naver.com>
2024-10-19 16:08:35 +0900

    Feat/Comment - Comment 조회시 빈값도 허용해야 하기 때문에 예외처리 제거

--- src/main/java/com/team5/pyeonjip/comment/service/CommentService.java
+++ src/main/java/com/team5/pyeonjip/comment/service/CommentService.java
@@ -18,11 +18,8 @@ public class CommentService {
     private final CommentRepository commentRepository;
 
     public List<Comment> getCommentsByProductId(Long productId) {
-        List<Comment> comments = commentRepository.findByProductId(productId);
-        if (comments.isEmpty()) {
-            throw new GlobalException(ErrorCode.COMMENT_NOT_FOUND);
-        }
-        return comments;
+        // 빈값도 허용을 해줘야 하기 때문에 예외처리 하면 안됨
+        return commentRepository.findByProductId(productId);
     }
 
     @Transactional
