8dd405d9d8218b30fa83003e9af073fc18b9d1ac
[CLOUD4] 송진욱 <wlsdnr19731@gmail.com>
2024-10-19 13:46:11 +0900

    Feat : 엔드포인트 메서드 수정

--- src/main/java/com/team5/pyeonjip/chat/controller/ChatMessageController.java
+++ src/main/java/com/team5/pyeonjip/chat/controller/ChatMessageController.java
@@ -4,10 +4,13 @@ import com.team5.pyeonjip.chat.dto.ChatMessageDto;
 import com.team5.pyeonjip.chat.service.ChatMessageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+
+import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,20 +20,29 @@ public class ChatMessageController {
     private final SimpMessagingTemplate messagingTemplate;
 
     @MessageMapping("/chat.sendMessage/{chatRoomId}")
-    public void sendMessage(@DestinationVariable("chatRoomId") Long chatRoomId, @Payload ChatMessageDto message) {
-        ChatMessageDto createdMessage = chatMessageService.sendMessage(chatRoomId, message.getMessage());
+    public void sendMessage(@DestinationVariable("chatRoomId") Long chatRoomId,
+                            @Payload ChatMessageDto message,
+                            @Header("simpUser") Principal principal) {
+        String senderEmail = principal.getName();
+        ChatMessageDto createdMessage = chatMessageService.sendMessage(chatRoomId, message.getMessage(), senderEmail);
         messagingTemplate.convertAndSend("/topic/messages/" + chatRoomId, createdMessage);
     }
 
     @MessageMapping("/chat.updateMessage/{chatRoomId}")
-    public void updateMessage(@DestinationVariable("chatRoomId") Long chatRoomId, @Payload ChatMessageDto message) {
-        ChatMessageDto updatedMessage = chatMessageService.updateMessage(message.getId(), message.getMessage());
+    public void updateMessage(@DestinationVariable("chatRoomId") Long chatRoomId,
+                              @Payload ChatMessageDto message,
+                              @Header("simpUser") Principal principal) {
+        String senderEmail = principal.getName();
+        ChatMessageDto updatedMessage = chatMessageService.updateMessage(message.getId(), message.getMessage(), senderEmail);
         messagingTemplate.convertAndSend("/topic/message-updates/" + chatRoomId, updatedMessage);
     }
 
     @MessageMapping("/chat.deleteMessage/{chatRoomId}")
-    public void deleteMessage(@DestinationVariable("chatRoomId") Long chatRoomId, @Payload ChatMessageDto message) {
-        Long deletedMessageId = chatMessageService.deleteMessage(message.getId());
+    public void deleteMessage(@DestinationVariable("chatRoomId") Long chatRoomId,
+                              @Payload ChatMessageDto message,
+                              @Header("simpUser") Principal principal) {
+        String senderEmail = principal.getName();
+        Long deletedMessageId = chatMessageService.deleteMessage(message.getId(), senderEmail);
         messagingTemplate.convertAndSend("/topic/message-deletions/" + chatRoomId, deletedMessageId);
     }
 }
