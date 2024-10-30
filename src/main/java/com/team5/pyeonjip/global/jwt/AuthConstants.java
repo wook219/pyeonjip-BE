2dbc3ab548dc10dfa44c63fb408731cfc3d17815
[CLOUD4] 현태훈 <winwin_th@naver.com>
2024-10-30 16:52:33 +0900

    Fix: 타 클래스에서 사용할 수 있도록 public, static으로 수정

--- src/main/java/com/team5/pyeonjip/global/jwt/AuthConstants.java
+++ src/main/java/com/team5/pyeonjip/global/jwt/AuthConstants.java
@@ -3,14 +3,14 @@ package com.team5.pyeonjip.global.jwt;
 public class AuthConstants {
 
     // Access Token을 전달할 헤더
-    private final String AUTH_HEADER = "Authorization";
+    public static final String AUTH_HEADER = "Authorization";
 
     // Access Token의 타입
-    private final String AUTH_TOKEN = "Bearer ";
+    public static final String AUTH_TOKEN = "Bearer ";
 
     // 각 Token의 유효 시간
-    private final Long REFRESH_TOKEN_EXPIRED_MS = 600000L;
-    private final Long ACCESS_TOKEN_EXPIRED_MS = 86400000L;
+    public static final Long REFRESH_TOKEN_EXPIRED_MS = 600000L;
+    public static final Long ACCESS_TOKEN_EXPIRED_MS = 86400000L;
 
 
 }
